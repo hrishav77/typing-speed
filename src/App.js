5a0a720e1f32e79e2b55ddb4a07dfaa495f62f4f
hrishav77 <f20210629@goa.bits-pilani.ac.in>
2022-09-10 15:36:23 +0530

    fixed word feature

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, {useEffect, useState} from "react";
-import Words from "./words.jsx";
+// import Words from "./words.jsx";
 import Header from "./header.jsx";
 import Footer from "./footer.jsx";
-import alot from "./words.jsx";
 import "./styles.css";
 
 
+
 function App() {
-
-
+ 
+  
   return (
     <div> 
     <Header/>
-    <Words />
+    
     <Footer/>
     </div>
 
--- src/header.jsx
+++ src/header.jsx
@@ -1,25 +1,54 @@
 import React, {useEffect, useState} from "react";
 import "./styles.css";
-import alot from "./words";
+import Array from "./dictionary";
 
 
 function Header(){
+    const [dig,setDig]=useState("");
+    const [no,setNo]=useState(10);
+    
+    let text=""
+    function random(num)
+    {
+    setNo(num)
+    console.log(no)
+    for(var i=0;i<no;i++){
+      var ind=Math.floor(Math.random()*Array.length);
+      if(i===0){
+        text=Array[ind];
+      }
+      else{
+        text=text+" "+Array[ind];
+        console.log(text)
+    }}
+    setDig(text)
+}
 
+  
     
     return(
-        <div className="nav-settings">
+     <>
+     
+     <div className="nav-settings">
         <a href="https://ibb.co/nzKYJ2n"><img src="https://i.ibb.co/9sd15XV/logo.png" alt="logo" border="0" width="60px" className="logo-settings"/></a>
         <h1>Typing...</h1>
+   
         <div className="mode">
-         {/* <p>Words:</p>
-         <a  className="design-mode" onClick={function(e) {alot(10)}}>10</a>
-         <a  className="design-mode" onClick={function(e) {alot(5)}}>15</a>
-         <a className="design-mode"  href="words.jsx">20</a>
-         <a  className="design-mode" href="words.jsx">30</a>
-         <a  className="design-mode" href="words.jsx">60</a> */}
+         <p>Words:
+         <a  className="design-mode" onClick={function(e) {random(10)}}>10</a>
+         <a  className="design-mode" onClick={function(e) {random(15)}}>15</a>
+         <a className="design-mode"  onClick={function(e) {random(20)}}>20</a>
+         <a  className="design-mode" onClick={function(e) {random(30)}}>30</a>
+         <a  className="design-mode" onClick={function(e) {random(60)}}>60</a></p>
         </div>
-       
-     </div>
+        </div>
+    <div className="area">
+    <form>
+    <textarea className="area" type="text" placeholder={dig}/>
+    </form>
+    </div> 
+     </>
     );
 }
 export default Header;
+
